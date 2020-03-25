8114f2f6f1517aecf8e395d638cd5357b25cccb8
Jeremy Huddleston Sequoia <jeremyhu@apple.com>
2020-03-25 11:44:58 -0700

    [macos] Set the preferred JVM type to zero for arm64
    Signed-off-by: Jeremy Huddleston Sequoia <jeremyhu@apple.com>

--- src/java.base/macosx/native/libjli/java_md_macosx.m
+++ src/java.base/macosx/native/libjli/java_md_macosx.m
@@ -210,6 +210,8 @@ static InvocationFunctions *GetExportedJNIFunctions() {
         preferredJVM = "client";
 #elif defined(__x86_64__)
         preferredJVM = "server";
+#elif defined(__arm64__)
+        preferredJVM = "zero";
 #else
 #error "Unknown architecture - needs definition"
 #endif
